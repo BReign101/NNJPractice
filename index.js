1bed45a4e6116cea53c98b46c55b9bee4fab805c
Reign101 <bh0477421@otc.edu>
2017-11-08 00:16:25 -0600

    My final push with color

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var _ = require('lodash');
-var chalk       = require('chalk');
+var chalk  = require('chalk');
 var jsonfile = require('jsonfile')
+console.log(chalk.blue('I')+chalk.green.bgRedBright('think')+chalk.cyan('chalk')+chalk.yellow('is'+chalk.blueBright('cool!!'))); 
 var file = 'data.json'
 var read =jsonfile.readFileSync(file, 'utf8');
   console.log(read);
